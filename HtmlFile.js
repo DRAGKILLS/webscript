222590f9245ca6214e2adeb50b4c2d330e79f40c
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-11-23 19:39:49 +0300

    If it works im the best

--- HtmlFile.js
+++ HtmlFile.js
@@ -1,6 +1,16 @@
 class HtmlFile {
     f() {
-        return "<!DOCTYPE html> <html> <title>Title</title> </head> <body> <h1>My First Heading</h1> <p>My first paragraph.</p> </body> </html>";
+        return `
+<!DOCTYPE html>
+<html>
+<title>Title</title>
+</head>
+<body>
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+</body>
+</html>
+`;
     }
 }
 
